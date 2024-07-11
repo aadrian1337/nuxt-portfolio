53379d0fbef317e4bc62209551e02f116017e1aa
Adryan <51015296+aadrian1337@users.noreply.github.com>
2024-07-11 15:18:30 +0200

    Pagination in Post.vue
    Added a condition to Post.vue that shows the pagination only on page containing "/blog".

--- components/Post.vue
+++ components/Post.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
+// Define props and other reactive data
 const props = defineProps(["posts"]);
 const itemsPerPage = 3; // Number of posts to show per page
 const currentPage = ref(1);
 
+// Define computed properties for pagination
 const paginatedPosts = computed(() => {
   const start = (currentPage.value - 1) * itemsPerPage;
   const end = start + itemsPerPage;
@@ -13,6 +15,7 @@ const paginatedPosts = computed(() => {
 
 const totalPages = computed(() => Math.ceil(props.posts.length / itemsPerPage));
 
+// Methods for navigating pages
 const nextPage = () => {
   if (currentPage.value < totalPages.value) {
     currentPage.value++;
@@ -24,6 +27,14 @@ const prevPage = () => {
     currentPage.value--;
   }
 };
+
+// Reactive state to store the result of the URL check
+const isBlogPage = ref(false);
+
+// Perform the URL check on mounted
+onMounted(() => {
+  isBlogPage.value = window.location.pathname.includes('/blog');
+});
 </script>
 
 <template>
@@ -38,18 +49,20 @@ const prevPage = () => {
         class="w-full h-48 object-cover" />
       <div class="p-6 flex gap-4 flex-col">
         <NuxtLink
-          :to="post._path" class="after:absolute after:left-0 after:right-0 after:top-0 after:bottom-0">
+          :to="post._path"
+          class="after:absolute after:left-0 after:right-0 after:top-0 after:bottom-0">
           <h2 class="text-xl font-bold mb-2">{{ post.title }}</h2>
         </NuxtLink>
         <p class="text-gray-700 mb-4">{{ post.description }}</p>
       </div>
     </div>
-    <div class="flex justify-center mr-auto col-start-1 col-span-1">
+    <div v-if="isBlogPage" class="flex justify-center mr-auto col-start-1 col-span-1">
       <button
         @click="prevPage"
         :disabled="currentPage === 1"
         class="px-4 py-2 bg-blue-500 text-white rounded-l-md disabled:opacity-50">
-Newer      </button>
+        Newer
+      </button>
       <button
         @click="nextPage"
         :disabled="currentPage === totalPages"
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,20 +1,17 @@
 <script setup>
 const { data: posts } = await useAsyncData("posts", () =>
-  queryContent("/blog").find()
+  queryContent("/blog").sort({ date: -1 }).find()
 );
 </script>
 
 <template>
-  <div>
-    <h1 class="text-4xl my-8">Blog Posts</h1>
+  <div class="gap-8 flex flex-col">
+    <h1 class="text-4xl mt-8">Blog Posts</h1>
 
-    <p>I like to write about anything that I am currently working or something new
-    that interests me. If you would like me to write about something or be a
-    guest blogger on your blog please reach out to me on Twitter. If you would
-    like to subscribe to an RSS feed you can find it here.</p>
+    <p class="max-w-2xl">I like to write about anything that I am currently working or something new that interests me. If you would like me to write about something or be a guest blogger on your blog please reach out to me on Twitter. If you would like to subscribe to an RSS feed you can find it here.</p>
 
     <section>
-        <Post :posts="posts" class="grid sm:grid-cols-2 lg:grid-cols-3 mt-8 gap-10"/>
+        <Post :posts="posts" class="grid sm:grid-cols-2 lg:grid-cols-3 gap-10"/>
     </section>
   </div>
 </template>
